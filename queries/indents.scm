a2423695d2587ad27271724c6b42708c9bc8d4b7
simonvic <simonvic.dev@gmail.com>
2026-04-14 15:18:15 +0200

    fix(query): if statement indentation without block

--- queries/indents.scm
+++ queries/indents.scm
@@ -22,6 +22,21 @@
   "}"
 ] @indent.end
 
+(if
+  condition: (_) @indent.begin)
+
+; Supports if without braces (but not both if-else without braces)
+(if
+  consequence: (_) @indent.end @_consequence
+  (#not-kind-eq? @_consequence "block")
+  ("else" @indent.branch
+    [
+      (if
+        (block) @indent.dedent)? @indent.dedent
+      (block)? @indent.dedent
+      (_)? @indent.dedent
+    ])?) @indent.begin
+
 (comment_line) @indent.ignore
 
 [

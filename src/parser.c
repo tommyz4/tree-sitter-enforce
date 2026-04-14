16f34a01ff259d744be4876b4e4c8f480a05e49b
simonvic <simonvic.dev@gmail.com>
2026-04-14 15:59:53 +0200

    chore: bump version

--- src/parser.c
+++ src/parser.c
@@ -47679,8 +47679,8 @@ TS_PUBLIC const TSLanguage *tree_sitter_enforce(void) {
     .max_reserved_word_set_size = 0,
     .metadata = {
       .major_version = 0,
-      .minor_version = 3,
-      .patch_version = 2,
+      .minor_version = 4,
+      .patch_version = 0,
     },
   };
   return &language;
